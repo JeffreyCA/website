ff5e46dbc41c6a6589c8c556428db54275726aee
Jeffrey <jeffreyca16@gmail.com>
2017-08-22 23:15:35 -0400

    Add sample cards

--- js/init.js
+++ js/init.js
@@ -2,10 +2,8 @@
     $(function () {
         $('.button-collapse').sideNav({
             closeOnClick: true
-        }
-        );
-
+        });
     }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-$('#about').click(function () { Materialize.toast('Version 0.0.1', 2000, 'rounded'); return false; });
+$('#about').click(function () { Materialize.toast('Version 0.0.1', 2000); return false; });
