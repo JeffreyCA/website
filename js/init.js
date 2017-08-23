9aa51646a1990a0a8d29d4b0a682ee1ad2400bac
Jeffrey <jeffreyca16@gmail.com>
2017-08-22 22:27:44 -0400

    Add account logo and background to mobile nav drawer

--- js/init.js
+++ js/init.js
@@ -1,7 +1,11 @@
 (function ($) {
     $(function () {
-
-        $('.button-collapse').sideNav();
+        $('.button-collapse').sideNav({
+            closeOnClick: true
+        }
+        );
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$('#about').click(function () { Materialize.toast('Version 0.0.1', 2000, 'rounded'); return false; });
