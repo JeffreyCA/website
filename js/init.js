d18db8f7add2321ff7344109fe0a8d19c32348e3
Jeffrey <jeffreyca16@gmail.com>
2017-08-24 22:56:58 -0400

    Implement swipeable tabs in showcase page

--- js/init.js
+++ js/init.js
@@ -3,6 +3,11 @@
         $('.button-collapse').sideNav({
             closeOnClick: true
         });
+
+        $('#showcase-tabs').tabs({
+            'swipeable': true
+        });
+
         $('#about').click(function () {
             Materialize.toast('Last updated: 2017-08-24', 2000);
             return false;
