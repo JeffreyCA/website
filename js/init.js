3257fb5e36f6ce51ee80cdba8811993e304ef3bc
Jeffrey <jeffreyca16@gmail.com>
2017-09-08 23:11:25 -0400

    Automatically update last-modified date and align chips

--- js/init.js
+++ js/init.js
@@ -1,3 +1,16 @@
+// Convert date to yyyy-mm-dd format
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
 (function ($) {
     $(function () {
         $('.button-collapse').sideNav({
@@ -9,8 +22,21 @@
         });
 
         $('#about').click(function () {
-            Materialize.toast('Last updated: 2017-08-27', 2000);
+            var formattedDate = formatDate(new Date(document.lastModified));
+            Materialize.toast('Last updated: ' + formattedDate, 2000);
             return false;
         });
+
+        $("#android-egg").hover(
+            // Mouse hover
+            function () {
+
+            },
+            // Mouse leave
+            function () {
+
+            }
+        );
+
     }); // end of document ready
 })(jQuery); // end of jQuery name space
